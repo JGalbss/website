b1914dfbad9802a9daf427e2c5e1d3c88aa408e8
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-02 12:47:14 -0400

    chore: update tsconfig paths, prettier config, init navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import localFont from 'next/font/local'
-import './globals.css'
+import localFont from 'next/font/local';
+
+import Navbar from '../components/common/navbar';
+import './globals.css';
 
 const charcoal = localFont({
   src: '../public/fonts/Charcoal.ttf',
   display: 'swap',
-})
+});
 
 export const metadata = {
   title: 'Gitswitch',
   description: 'Super sick site by Gitswitch',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={charcoal.className}>{children}</body>
+      <body className={charcoal.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/common/navbar.tsx
@@ -0,0 +1,19 @@
+
+
+/* Component */
+const Navbar = () => {
+    return (
+        <nav className="flex">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+Navbar.displayName = 'Navbar'
+export default Navbar
+
