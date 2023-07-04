dd2e02d3225cae3cd22412b36b7f8301b15bc1ea
Josh Galbreath <joshsgalbreath@gmail.com>
2023-07-04 12:23:49 -0400

    chore: add layout and navbar placeholder

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,19 +1,16 @@
-
-
 /* Component */
 const Navbar = () => {
-    return (
-        <nav className="flex">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex w-full bg-gray-11 p-2">
+      <ul className="flex space-x-4 text-gray-1">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
 
-Navbar.displayName = 'Navbar'
-export default Navbar
-
+Navbar.displayName = 'Navbar';
+export default Navbar;
